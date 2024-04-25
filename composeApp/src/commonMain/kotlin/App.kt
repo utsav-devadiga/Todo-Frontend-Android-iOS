009fb9e5240bb19f6466f9ef357c7cd0525e80f3
utsav-devadiga <utsavdevadiga10@gmail.com>
2024-04-25 23:51:59 +0530

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,100 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.auth.FirebaseUser
+import dev.gitlive.firebase.auth.auth
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        val scope = rememberCoroutineScope()
+        val auth = remember { Firebase.auth }
+
+        var firebaseUser: FirebaseUser? by remember { mutableStateOf(null) }
+        var userEmail by remember { mutableStateOf("") }
+        var userPassword by remember { mutableStateOf("") }
+
+
+        if (firebaseUser == null) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextField(
+                    value = userEmail,
+                    onValueChange = { userEmail = it },
+                    placeholder = { Text(text = "Please enter email address") },
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                TextField(
+                    value = userPassword,
+                    onValueChange = { userPassword = it },
+                    placeholder = { Text(text = "Please enter password") },
+                    visualTransformation = PasswordVisualTransformation()
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Button(
+                    onClick = {
+                        scope.launch {
+                            try {
+                                firebaseUser = auth.createUserWithEmailAndPassword(
+                                    email = userEmail,
+                                    password = userPassword
+                                ).user
+                            } catch (e: Exception) {
+                                firebaseUser = auth.signInWithEmailAndPassword(
+                                    email = userEmail,
+                                    password = userPassword
+                                ).user
+                            }
+                        }
+                    }) {
+                    Text(text = "Sign in")
+                }
+            }
+        } else {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = firebaseUser!!.uid)
+                Spacer(modifier = Modifier.height(12.dp))
+                Button(
+                    onClick = {
+                        scope.launch {
+                            auth.signOut()
+                            firebaseUser = null
+                        }
+                    }) {
+                    Text(text = "Sign out")
+                }
+            }
+        }
+
+    }
+}
